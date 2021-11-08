b65e616202ccf9c231eba5818531ecc80086fa75
Fabio Souza <fabiovanderlei.developer@gmail.com>
2021-11-08 13:55:24 -0300

    fix: add ZSHRC env

--- customizations/install.sh
+++ customizations/install.sh
@@ -42,5 +42,8 @@ else
   exit 1
 fi
 
+echo 'nexport ZSHRC=$HOME/.zshrc' | tee -a $HOME/.zshrc
+
+
 #echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 #          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm ' | tee -a $HOME/.zshrc

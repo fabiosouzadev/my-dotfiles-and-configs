16778377ae22108b7119603eea05a951677f9265
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-01-17 12:15:16 -0300

    fix: fix all config and plugins

--- zshrc.d/001-aliases.zsh
+++ zshrc.d/001-aliases.zsh
@@ -1,1 +1,38 @@
-alias lll='ls -alh'
+## CHECK IF NVIM
+if [ -x /bin/nvim ]; then
+    alias vi="/bin/nvim"
+    alias vim="/bin/nvim"
+    export EDITOR="/bin/nvim"
+fi
+
+if [ -x /usr/bin/nvim ]; then
+    alias vi="/usr/bin/nvim"
+    alias vim="/usr/bin/nvim"
+    export EDITOR="/usr/bin/nvim"
+fi
+
+if [ -x /opt/local/bin/nvim ]; then
+    alias vim="/opt/local/bin/nvim"
+    alias vi="/opt/local/bin/nvim"
+    export EDITOR="/opt/local/bin/nvim"
+fi
+if [ -x /usr/local/bin/nvim ]; then
+    alias vim="/usr/local/bin/nvim"
+    alias vi="/usr/local/bin/nvim"
+    export EDITOR="/usr/local/bin/nvim"
+fi
+
+# Clearly, I also really like nvim.
+
+#ALIAS
+alias zc="${EDITOR}  ~/.zshrc"
+alias hc="sudo ${EDITOR} /etc/hosts"
+alias ls="exa"
+alias lll='exa -alh'
+
+# Pretty print the path
+alias path='echo $PATH | tr -s ":" "\n"'
+
+#Lazygit and Lazydocker
+alias lg="lazygit"
+alias ld="lazydocker"

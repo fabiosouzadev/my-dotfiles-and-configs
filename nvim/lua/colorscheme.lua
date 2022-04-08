f9862b71aa3752ee10c2305c37fa1561c660749e
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-04-08 18:56:11 -0300

    refactor: add colorscheme, lualine, nvimtree, telescope

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -2,3 +2,4 @@ local opt = vim.opt
 
 -- Theme
 opt.termguicolors = true
+vim.cmd[[colorscheme tokyonight]]
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,24 +1,77 @@
--- Plugins
+-- https://github.com/ChristianChiarulli/nvim/blob/master/lua/user/plugins.lua
+
+-- Plugins   
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer close and reopen Neovim...")
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
   vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
+vim.cmd [[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]])
+]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  
+  -- Core
+  -- Nvim Tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    config = function() require'nvim-tree'.setup {} end
+  }
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- LSP
+
+  -- Autocompletion and Snippets
+
+   -- Theme/Syntax
+   use 'folke/tokyonight.nvim'
+   use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

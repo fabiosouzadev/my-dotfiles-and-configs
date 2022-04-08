ea6d297baad3a2e7dfe1882acc908dcf643e9785
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-04-08 11:34:25 -0300

    refactor: add moving key to new init.lua

---
+++ nvim/lua/colorscheme.lua
@@ -0,0 +1,4 @@
+local opt = vim.opt
+
+-- Theme
+opt.termguicolors = true
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,40 @@
+local opt = vim.opt
+local o = vim.o
+
+-- General
+opt.compatible = false
+opt.mouse = 'a'
+opt.clipboard = 'unnamedplus'
+opt.swapfile = false
+opt.undodir = vim.fn.stdpath('data') .. '/undodir'
+opt.backup = false
+opt.showmode = false
+opt.hidden = true
+opt.history = 100
+opt.lazyredraw = true
+opt.ttyfast = true
+opt.synmaxcol = 240
+opt.errorbells = false
+opt.showcmd = true
+opt.completeopt = 'menuone,noinsert,noselect'
+opt.wildignore:append {'*/node_modules/**'}
+
+-- Editor
+opt.number = true
+opt.relativenumber = true
+opt.wrap = false
+opt.smartindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.scrolloff=2
+opt.showmode = false
+--  opt.signcolumn = 'yes'
+opt.splitright = true
+opt.splitbelow = true
+
+-- Set the behavior of tab
+opt.tabstop=2              
+opt.softtabstop=2         
+opt.shiftwidth=4             
+opt.expandtab=true

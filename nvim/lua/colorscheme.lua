95eba190bfb718209c5bde82bf89f889dd508cca
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-04-15 17:20:56 -0300

    refactor: telescope keys, theme, tmux, nvim-treesitter

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,5 +1,13 @@
 local opt = vim.opt
+local g = vim.g
 
 -- Theme
 opt.termguicolors = true
+
+g.tokyonight_style = "storm" --storm, night or day
+g.tokyonight_italic_functions = true
+g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the hint color to the 'orange' color, and make the 'error' color bright red
+g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 vim.cmd[[colorscheme tokyonight]]
--- nvim/lua/plugins/configs/nvim-treesitter.lua
+++ nvim/lua/plugins/configs/nvim-treesitter.lua
@@ -23,19 +23,5 @@ require'nvim-treesitter.configs'.setup {
   highlight = { 
     enable = true,
     additional_vim_regex_highlighting = true
-  }, 
-  incremental_selection = { 
-    enable = true 
-  }, 
-  textobjects = { 
-    enable = true
-  },
-  context_commentstring = {
-    enable = true
-  },
-  git = {
-      enable = true,
-      ignore = true,
-      timeout = 500,
-  },
+  }
 }

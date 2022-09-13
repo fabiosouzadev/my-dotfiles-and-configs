dd507277df5141ceff9bc49350deed45aa7795fe
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-09-13 11:49:26 -0300

    fix: remove espace

--- zshrc.d/001-aliases.zsh
+++ zshrc.d/001-aliases.zsh
@@ -40,6 +40,3 @@ alias ld="lazydocker"
 
 alias showhidden="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
 alias hidehidden="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
-
-
-

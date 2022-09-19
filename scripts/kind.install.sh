043390dc13e1731540c0e5ce15bd2f7d410ffe13
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-09-19 16:47:15 -0300

    feat: add kind (k8s - local)

---
+++ scripts/kind.install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ `uname` = 'Linux' ]; then
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
+    chmod +x -v ./kind
+    sudo mv -v ./kind /usr/local/bin/kind
+elif [ `uname` = 'Linux' ]; then
+    brew install kind
+fi

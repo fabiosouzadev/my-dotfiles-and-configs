06fadb5d1321537dd54685232d76d6f5ba2aed04
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-09-23 12:56:54 -0300

    fix: fix install kind in macos

--- scripts/kind.install.sh
+++ scripts/kind.install.sh
@@ -3,6 +3,6 @@ if [ `uname` = 'Linux' ]; then
     curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
     chmod +x -v ./kind
     sudo mv -v ./kind /usr/local/bin/kind
-elif [ `uname` = 'Linux' ]; then
+elif [ `uname` = 'Darwin' ]; then
     brew install kind
 fi

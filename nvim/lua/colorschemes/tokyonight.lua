84b0dd9d8a6f9b05215627053800503896bce904
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-11-11 15:18:55 -0300

    chore,fix: fix some configs and apply tokyonight-moon

--- nvim/lua/colorschemes/tokyonight.lua
+++ nvim/lua/colorschemes/tokyonight.lua
@@ -5,10 +5,23 @@ local opt = vim.opt
 opt.termguicolors = true
 
 -- Theme
-g.tokyonight_style = "storm" --storm, night or day
+g.tokyonight_style = "moon" --storm, night, moon or day
 g.tokyonight_italic_functions = true
 g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- Change the hint color to the 'orange' color, and make the 'error' color bright red
 g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.cmd[[colorscheme tokyonight]]
+
+require("tokyonight").setup({
+  -- use the night style
+  style = "moon",
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  lualine_bold = true,
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.error = "#ff0000"
+  end
+})
+
+vim.cmd[[colorscheme tokyonight-moon]]

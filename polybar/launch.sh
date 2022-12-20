708b33de0f8b57821d25e2fb3db8b90894812673
Fabio Souza <fabiovanderlei.devloper@gmail.com>
2022-12-20 18:02:19 -0300

    feat: create poweroff button on polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,5 +3,5 @@
 #! /bin/sh
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -c $HOME/.config/polybar/config.ini example
+polybar -c $HOME/.config/polybar/config.ini main
 sleep 1

907cf7693bfdb0ef475c71346817229865ec9aaa
Fabio Souza <fabiovanderlei.devloper@gmail.com>
2022-12-20 10:31:01 -0300

    feat: polybar working

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+#! /bin/sh
 killall -q polybar
-
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar mybar
-
+polybar -c $HOME/.config/polybar/config.ini example
+sleep 1

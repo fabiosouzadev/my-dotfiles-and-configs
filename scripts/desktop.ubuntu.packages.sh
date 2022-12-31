caba1ae470c12f39e42918b03d86a6f0a3063c82
Fabio Souza <fabiovanderlei.developer@gmail.com>
2022-12-31 12:21:04 -0300

    feat: add Librewolf browser *commented*

--- scripts/desktop.ubuntu.packages.sh
+++ scripts/desktop.ubuntu.packages.sh
@@ -8,7 +8,7 @@ sudo apt install curl
 #Add to sources list
 #Brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
 
 
 

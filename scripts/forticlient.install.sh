ef47efcb405b59023d7af8d373bfebf7a29220d4
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-01-05 16:58:28 -0300

    fix: forticlient for 20.04, and add tmux sessions plugins

--- scripts/forticlient.install.sh
+++ scripts/forticlient.install.sh
@@ -1,4 +1,10 @@
-wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo apt-key add -
-echo 'deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse' |  sudo tee /etc/apt/sources.list.d/forticlient.list 
+#wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo apt-key add -
+#echo 'deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse' |  sudo tee /etc/apt/sources.list.d/forticlient.list 
+
+
+wget -O - https://repo.fortinet.com/repo/6.4/ubuntu/DEB-GPG-KEY | sudo apt-key add -
+echo 'deb [arch=amd64] https://repo.fortinet.com/repo/6.4/ubuntu/ /bionic multiverse' |  sudo tee /etc/apt/sources.list.d/forticlient.list 
+
+
 sudo apt-get update
 sudo apt install -y forticlient

4036f8acbead7712c6f10d775f4d4531d27fdb16
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-02-06 10:12:24 -0300

    feat: improve lualine -> lazy.nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,25 +1,17 @@
 -- Status Line and Bufferline
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    { "kyazdani42/nvim-web-devicons", lazy = true },
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "moonfly",
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
-      },
-      sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename','lsp_progress'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-      },
-    })
-  end,
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons", lazy = true },
+    },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "moonfly",
+                component_separators = "|",
+                section_separators = "",
+            },
+        })
+    end,
 }

330ce1cbd2d832a2ed4315503599af559cf6daa1
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-02-06 10:11:30 -0300

    feat: fix treesitter -> lazy.nvim

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- run = ":TSUpdate",
+        config = function()
+            local servers = require("lsp.ensure_installed")
+            require("nvim-treesitter.configs").setup({
+                highlight = {
+                    enable = true, -- false will disable the whole extension
+                    additional_vim_regex_highlighting = true,
+                },
+                indent = { enable = true },
+                autopairs = {
+                    enable = true,
+                },
+                -- enable autotagging (w/ nvim-ts-autotag plugin)
+                autotag = { enable = true },
+                -- ensure these language parsers are installed
+                ensure_installed = servers.ensure_installed_treesitter,
+                -- auto install above language parsers
+                auto_install = true,
+                incremental_selection = {
+                    enable = true,
+                },
+                textobjects = {
+                    enable = true,
+                },
+                -- context_commentstring = {
+                --   enable = true,
+                --   enable_autocmd = false,
+                -- },
+            })
+        end,
+        cmd = { "TSInstall", "TSUninstall", "TSUpdate", "TSInstallInfo", "TSInstallSync", "TSInstallFromGrammar" },
+        event = "User FileOpened",
+    },
+    {
+        -- Lazy loaded by Comment.nvim pre_hook
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        lazy = true,
+    },
+}

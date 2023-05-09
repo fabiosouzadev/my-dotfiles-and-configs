154e037bb6cb2c09ce9163e23cca4d78394331cc
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-05-09 19:15:14 -0300

    fix: fix to ubuntu install

---
+++ zshrc.d/018-asdf.zsh
@@ -0,0 +1,6 @@
+export PATH="$PATH:$HOME/.asdf/bin"
+. "$HOME/.asdf/asdf.sh"
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit

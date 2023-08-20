2caf111c4922ab23b1dd9f4ccffed035072193a5
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-08-20 15:01:22 -0300

    feat (phpenv): remove phpenv configs

--- zshrc.d/021-phpenv.zsh
+++
@@ -1,7 +0,0 @@
-export PHPENV_ROOT="$HOME/.phpenv"
-command -v phpenv > /dev/null || export PATH="$PHPENV_ROOT/bin:$PATH"
-if command -v phpenv 1>/dev/null 2>&1; then 
-  eval "$(phpenv init -)" 
-fi
-
-# export PYENV_VIRTUALENV_DISABLE_PROMPT=1

067add0bd53ab5bd1ebfd91b498dae258a657415
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-09-12 09:58:10 -0300

    fix(ubuntu): fix ubuntu install

--- zshrc.d/014-gcloud.zsh
+++ zshrc.d/014-gcloud.zsh
@@ -1,7 +1,5 @@
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 #export PATH=$HOME/.local/src/google-cloud-sdk/bin:$PATH
-
-
 # OBS: Not working completion in ~/.local/share/zsh/site-functions/
 source $HOME/.local/src/google-cloud-sdk/path.zsh.inc
 autoload -U compinit compdef

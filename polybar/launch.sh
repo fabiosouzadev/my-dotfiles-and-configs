e53df768a7ec65d069dbf8e1c2ac251ec5c96b0b
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-09-23 12:11:34 -0300

    feat(i3,polybar): add polybar configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#! /bin/sh
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -c $HOME/.config/polybar/config.ini main
-sleep 1
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
+
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+            
+	# Launch the bar
+    if type "xrandr"; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar -q main -c "$dir/$style/config.ini" &
+        done
+    else
+	    polybar -q main -c "$dir/$style/config.ini" &
+    fi
+    # polybar -q main -c "$dir/$style/config.ini" &
+    sleep 1
+}
+
+if [[ "$1" == "--one" ]]; then
+	style="one"
+	launch_bar
+
+elif [[ "$1" == "--two" ]]; then
+	style="two"
+	launch_bar
+
+else
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	 --one    --two
+	# --blocks    --colorblocks    --cuts      --docky
+	# --forest    --grayblocks     --hack      --material
+	# --panels    --pwidgets       --shades    --shapes
+	EOF
+fi

8db6527758dce821c831253708abafe14f440b12
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-10-03 11:53:28 -0300

    feat(picom) added picom build and picom animations

--- monitors/xrand/dual.sh
+++ monitors/xrand/dual.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 2560x1080 --pos 1366x0 --rotate normal --output DP-1 --off && \
-feh --bg-fill --randomize $HOME/.config/wallpapers
+xrandr --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --rate 60 --primary --mode 2560x1080 --pos 1366x0 --rotate normal --output DP-1 --off

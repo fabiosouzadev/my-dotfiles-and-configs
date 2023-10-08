c8efa09fa55a6aca27b2f71df94617fed99f44ca
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-10-08 15:43:29 -0300

    fix (xubuntu-i3, k9s): fixed k9s

---
+++ zshrc.d/016-k9s.zsh
@@ -0,0 +1,5 @@
+export TERM=xterm-256color
+# Kubectl edit command will use this env var.
+export EDITOR=nvim
+# Should your editor deal with streamed vs on disk files differently, also set...
+export K9S_EDITOR=nvim

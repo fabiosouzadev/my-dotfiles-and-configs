81abf9648814d68b593893e6ff2080412d6d0322
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-10-10 14:20:57 -0300

    chore(monitors): added monitors configs xfce

--- monitors/xrand/dual.sh
+++ monitors/xrand/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --rate 60 --primary --mode 2560x1080 --pos 1366x0 --rotate normal --output DP-1 --off
+xrandr --output eDP-1 --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 2560x1080 --pos 1366x0 --rotate normal --output DP-1 --off

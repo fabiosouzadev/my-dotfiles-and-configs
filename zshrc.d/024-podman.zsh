ca389afb40d911606f4578b6fd9652c866f3a48f
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-10-29 16:13:25 -0300

    feat(podman): added podman configs

--- zshrc.d/024-podman.zsh
+++ zshrc.d/024-podman.zsh
@@ -5,3 +5,8 @@ fi
 if command -v podman-compose 1>/dev/null 2>&1; then 
     alias docker-compose=podman-compose
 fi
+
+
+export KIND_EXPERIMENTAL_PROVIDER=podman
+export CONTAINER_HOST=/var/run/docker.sock podman ps
+export DOCKER_HOST=unix:///run/podman/podman.sock

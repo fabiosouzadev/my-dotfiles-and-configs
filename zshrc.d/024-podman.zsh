3e930dcd34bc34faf52dbcb427afc64cda2c271b
Fabio Souza <fabiovanderlei.developer@gmail.com>
2023-10-30 16:34:37 -0300

    chore(podman): changed podman configs

--- zshrc.d/024-podman.zsh
+++ zshrc.d/024-podman.zsh
@@ -1,12 +1,12 @@
+alias "podman"="sudo podman" 
 if command -v podman 1>/dev/null 2>&1; then 
-    alias docker=podman
+    alias docker="sudo podman"
 fi
 
 if command -v podman-compose 1>/dev/null 2>&1; then 
-    alias docker-compose=podman-compose
+    alias docker-compose="podman-compose"
 fi
 
 
 export KIND_EXPERIMENTAL_PROVIDER=podman
-export CONTAINER_HOST=/var/run/docker.sock podman ps
 export DOCKER_HOST=unix:///run/podman/podman.sock

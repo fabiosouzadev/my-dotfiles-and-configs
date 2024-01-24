1f57e7913df023675fb4fddcdfeae3514f68239e
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-01-24 17:42:28 -0300

    chore(monitors): add configs
    Signed-off-by: Fabio Souza <fabiovanderlei.developer@gmail.com>

--- monitors/xrand/dual_intel_iris_xe.sh
+++ monitors/xrand/dual_intel_iris_xe.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 2560x1080x49.94 --pos 1920x0 --rotate normal
+feh --bg-fill --randomize $HOME/.config/wallpapers
